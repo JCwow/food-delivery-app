a32946c4e228df4cb95e4ed381aa8fbf7166c4c4
Jie-Si Chen <jessie@paxai.com>
2026-02-13 12:33:16 +0800

    implement search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { images } from '@/constants';
+import { router, useLocalSearchParams } from 'expo-router';
+import React, { useState } from 'react';
+import { Image, TextInput, TouchableOpacity, View } from 'react-native';
 const SearchBar = () => {
-  return (
-    <View>
-      <Text>SearchBar</Text>
-    </View>
-  )
+    const params = useLocalSearchParams<{query?: string}>();
+    const [query, setQuery] = useState(params.query ?? '');
+    // const debouncedSearch = useDebouncedCallback(
+    //     (text: string) => router.push(`/search?query=${text}`),
+    //     500
+    // );
+    const handleSearch = (text: string) => {
+        setQuery(text);
+        if (!text.trim()) router.setParams({ query: undefined });
+    };
+    const handleSubmit = () => {
+        const trimmedQuery = query.trim();
+        if (trimmedQuery) router.setParams({ query: trimmedQuery });
+    };
+    return (
+        <View className="searchbar">
+            <TextInput
+                className="flex-1 p-5"
+                placeholder='Search for pizzas, burgers ...'
+                value={query}
+                onChangeText={handleSearch}
+                onSubmitEditing={handleSubmit}
+                placeholderTextColor={"#A0A0A0"}
+                returnKeyType='search'>
+                
+            </TextInput>
+            <TouchableOpacity className="pr-5" onPress={handleSubmit}>
+                <Image
+                    source={images.search}
+                    className="size-6"
+                    resizeMode='contain'
+                    tintColor={"#5D5F6D"}
+                ></Image>
+            </TouchableOpacity>
+        </View>
+    )
 }
 
 export default SearchBar
